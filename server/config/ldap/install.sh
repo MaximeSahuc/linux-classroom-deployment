2a53eb163005736dcbb467946322bb3c34fa26a3
marccambon <marccambonm@gmail.com>
2025-04-23 15:56:41 +0200

    add ldif file in install.sh

--- server/config/ldap/install.sh
+++ server/config/ldap/install.sh
@@ -2,3 +2,8 @@
 
 sudo apt update
 sudo apt install requirements.txt
+
+ldapadd -x -D "cn=admin,dc=meg,dc=corp" -W -f base.ldif
+ldapadd -x -D "cn=admin,dc=meg,dc=corp" -W -f users.ldif
+ldapadd -x -D "cn=admin,dc=meg,dc=corp" -W -f groups.ldif
+
